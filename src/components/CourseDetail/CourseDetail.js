19cb0e87e26a2f0abed75e04d31ad2cceb2d6be6
Subroto Karmokar <subroto35-1734@diu.edu.bd>
2021-10-04 21:27:25 +0600

    all course button added

--- src/components/CourseDetail/CourseDetail.js
+++ src/components/CourseDetail/CourseDetail.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { Card, Col, Container, Row, Button } from 'react-bootstrap';
-import { BadgeCc, CalendarCheck, Globe, Heart, Share } from 'react-bootstrap-icons';
+import { ArrowBarLeft, BadgeCc, CalendarCheck, Globe, Heart, Share } from 'react-bootstrap-icons';
 import { useParams } from 'react-router';
+import { Link } from 'react-router-dom';
 import StarRatings from 'react-star-ratings';
 import useCourses from '../../hooks/useCourses';
 
@@ -23,7 +24,7 @@ const CourseDetail = () => {
         <Container className="mt-5">
             <Row>
                 <Col lg="8">
-                    <h1 style={{color: '#842029'}}>{course?.title}</h1>
+                    <h1 style={{ color: '#842029' }}>{course?.title}</h1>
                     <div className="d-flex">
                         <StarRatings
                             rating={course?.rating}
@@ -43,18 +44,27 @@ const CourseDetail = () => {
                         <span><BadgeCc /> English, French [Auto]</span>
                     </div>
                     <div className="my-5">
-                        <Button variant="outline-secondary" className="me-4 mb-2">Whitelist <Heart/> </Button>
-                        <Button variant="outline-secondary" className="me-4 mb-2">Share <Share/> </Button>
-                        <Button variant="outline-secondary">Gift the course</Button>
+                        <Button variant="outline-secondary" className="me-2 mb-2">Whitelist <Heart /> </Button>
+                        <Button variant="outline-secondary" className="me-2 mb-2">Share <Share /> </Button>
+                        <Button variant="outline-secondary" className="me-2 mb-2">Gift the course</Button>
+                    </div>
+                    <div>
+                        <Link to='/courses'>
+                            <Button
+                                variant="success"
+                                className="mb-2"
+                                style={{ backgroundColor: '#D32F2F', border: 0 }}
+                            ><ArrowBarLeft /> All Course</Button>
+                        </Link>
                     </div>
                 </Col>
                 <Col lg="4">
                     <Card className="">
-                        <Card.Img variant="top" src={course?.img}  />
+                        <Card.Img variant="top" src={course?.img} />
                         <Card.Body className="text-center d-flex justify-content-between">
                             <h4 className="text-secondary">Price: ${course?.price}</h4>
-                            <Button 
-                                variant="success" 
+                            <Button
+                                variant="success"
                                 style={{ backgroundColor: '#D32F2F', border: 0 }}
                             >Add to cart</Button>
                         </Card.Body>
